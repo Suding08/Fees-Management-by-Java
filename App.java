ccc81fce26059f55a5c970e404339be241777ee2
Suding08 <gsudin52@gmail.com>
2025-08-26 14:05:34 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.st_project_fees_management_system;
+
+/**
+ *
+ * @author gsudi
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
